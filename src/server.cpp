ff5223af2259fe3720cfaa61d059286efc690730
TanF12 <147226263+TanF12@users.noreply.github.com>
2023-11-18 23:32:22 -0600

    Formatação de código mais consistente

--- src/server.cpp
+++ src/server.cpp
@@ -7,16 +7,19 @@
 
 #define MAX_CONNECTIONS 10
 
-SimpleWebServer::SimpleWebServer(int port) : port(port) {
+SimpleWebServer::SimpleWebServer(int port) : port(port) 
+{
     setupSocket();
 }
 
-SimpleWebServer::~SimpleWebServer() {
+SimpleWebServer::~SimpleWebServer() 
+{
     close(server_socket);
 }
 
-void SimpleWebServer::setupSocket() {
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+void SimpleWebServer::setupSocket() 
+{
+    if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         handle_error("Erro ao criar socket");
     }
 
@@ -25,26 +28,31 @@ void SimpleWebServer::setupSocket() {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    if (bind(server_socket, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
+    if (bind(server_socket, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr)) < 0) 
+    {
         handle_error("Erro ao vincular o socket");
     }
 
-    if (listen(server_socket, MAX_CONNECTIONS) < 0) {
+    if (listen(server_socket, MAX_CONNECTIONS) < 0) 
+    {
         handle_error("Erro ao escutar por conexões");
     }
 
     std::cout << "Servidor escutando na porta " << port << "...\n";
 }
 
-void SimpleWebServer::start() {
+void SimpleWebServer::start() 
+{
     sockaddr_in client_addr;
     socklen_t addr_size = sizeof(client_addr);
     char buffer[1024] = {0};
     char* response = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, world!";
 
-    while (1) {
+    while(1) 
+    {
         int new_socket;
-        if ((new_socket = accept(server_socket, reinterpret_cast<struct sockaddr*>(&client_addr), &addr_size)) < 0) {
+        if((new_socket = accept(server_socket, reinterpret_cast<struct sockaddr*>(&client_addr), &addr_size)) < 0) 
+        {
             handle_error("Erro ao aceitar conexão");
         }
 
